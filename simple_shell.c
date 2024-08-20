05b5d07e0eda2b231689fa9499ade991ccbd99fa
HansSora <hansisoraa@gmail.com>
2024-08-20 08:34:27 +0000

    changes

--- simple_shell.c
+++ simple_shell.c
@@ -1,6 +1,38 @@
 #include "shell.h"
 
 /**
+ * command_read - Reads a command from stdin and executes it.
+ * @s: The command to read.
+ * Return: 0 on success, 1 on failure, 2 to indicate exit.
+ */
+int command_read(char *s)
+{
+    char *cmd_array[100];
+    char *token;
+    int i = 0;
+
+    if (strcmp(s, "exit") == 0)
+        return (2);
+    if (strcmp(s, "env") == 0)
+        return (_printenv());
+
+    token = strtok(s, " ");
+    while (token != NULL && i < 100)
+    {
+        cmd_array[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+    cmd_array[i] = NULL;
+
+    // Execute the first command in cmd_array
+    if (cmd_array[0] != NULL)
+        return (execute(cmd_array[0]));
+
+    return (1);
+}
+
+/**
  * execute - Executes a command.
  * @cmd: The command to execute.
  * Return: 0 on success, 1 on failure.
@@ -56,40 +88,4 @@ int main(void)
         fflush(stdout);
 
         /* Read the command from standard input */
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            /* Handle end of file (Ctrl+D) */
-            if (feof(stdin))
-            {
-                printf("\n");
-                free(line);
-                exit(EXIT_SUCCESS);
-            }
-            else
-            {
-                perror("getline");
-                free(line);
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        /* Remove newline character from the end of the line */
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
-
-        /* If the command is empty, just display the prompt again */
-        if (strlen(line) == 0)
-            continue;
-
-        /* Execute the command */
-        if (execute(line) != 0)
-        {
-            fprintf(stderr, "./shell: No such file or directory\n");
-        }
-    }
-
-    /* Clean up */
-    free(line);
-    return 0;
-}
+        nrea
