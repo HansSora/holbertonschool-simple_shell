fbfbaa64a7b6c082f82bda16ea5f2c826d5e8174
HansSora <hansisoraa@gmail.com>
2024-08-22 13:11:58 +0000

    changes

--- environment_utils.c
+++ environment_utils.c
@@ -1,74 +1,72 @@
 #include "main.h"
 
 /**
- * _printenv - Prints the environment
- * @void: void
+ * _printenv - Prints the environment variables
  * Return: 0 on success, -1 on failure
  */
-
 int _printenv(void)
 {
-	int i = 0;
-	char *str = environ[0];
+    int i = 0;
 
-	while (str != NULL)
-	{
-		printf("%s\n", str);
-		i++;
-		str = environ[i];
-	}
-	return (0);
+    while (environ[i] != NULL)
+    {
+        printf("%s\n", environ[i]);
+        i++;
+    }
+
+    return (0);
 }
 
 /**
  * _getenv - Gets the value of an environment variable
  * @var: The environment variable to get
- * Return: The value of the environment variable
+ * Return: The value of the environment variable or NULL if not found
  */
-
 char *_getenv(char *var)
 {
-	int i, len_var;
+    int i, len_var;
 
-	len_var = strlen(var);
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		if (strncmp(environ[i], var, len_var) == 0)
-			return (&environ[i][len_var]);
-	}
-	return (NULL);
+    len_var = strlen(var);
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        if (strncmp(environ[i], var, len_var) == 0 && environ[i][len_var] == '=')
+            return (&environ[i][len_var + 1]);
+    }
+    return (NULL);
 }
 
 /**
  * trim_whitespace - Trims leading and trailing whitespace from a string
  * @str: The string to trim
  */
-
 void trim_whitespace(char *str)
 {
-	int i, j;
-	int len = strlen(str);
+    int i, j;
+    int len = strlen(str);
 
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	if (i > 0)
-	{
-		j = 0;
-		while (str[i] != '\0')
-		{
-			str[j] = str[i];
-			i++;
-			j++;
-		}
-		str[j] = '\0';
-	}
-	len = strlen(str);
-	i = len - 1;
-	while (i >= 0 && (str[i] == ' ' || str[i] == '\t'))
-	{
-		str[i] = '\0';
-		i--;
-	}
+    // Trim leading whitespace
+    i = 0;
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+    
+    if (i > 0)
+    {
+        j = 0;
+        while (str[i] != '\0')
+        {
+            str[j] = str[i];
+            i++;
+            j++;
+        }
+        str[j] = '\0';
+    }
 
+    // Trim trailing whitespace
+    len = strlen(str);
+    i = len - 1;
+    while (i >= 0 && (str[i] == ' ' || str[i] == '\t'))
+    {
+        str[i] = '\0';
+        i--;
+    }
 }
