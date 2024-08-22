ba38038ce2d060baa42ceb7896267912c64c0dfc
HansSora <hansisoraa@gmail.com>
2024-08-22 13:15:33 +0000

    changes

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@ int command_read(char *s)
     char *cmd_array[100];
 
     if (strcmp(s, "exit") == 0)
-        return (2);  // Correctly return 2 to indicate exit
+        return (2);
 
     if (strcmp(s, "env") == 0)
         return (_printenv());
